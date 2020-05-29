4e189d38cdb66f9695470fbba53654cc2d485111
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2020-05-30 01:31:35 +0300

    add averageRating to Restaurant component

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 import Menu from './menu';
 import Reviews from './reviews';
+import Rate from './rate';
 
-export default function Restaurant({ restaurant }) {
+export default function Restaurant({ restaurant: { menu, reviews } }) {
+  const averageRating =
+    reviews.reduce((acc, { rating }) => acc + rating, 0) / reviews.length;
   return (
     <div>
-      <Menu menu={restaurant.menu} />
-      <Reviews reviews={restaurant.reviews} />
+      <Rate rating={averageRating.toFixed(2)} />
+      <Menu menu={menu} />
+      <Reviews reviews={reviews} />
     </div>
   );
 }
