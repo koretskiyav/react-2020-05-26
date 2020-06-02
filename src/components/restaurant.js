847046d6248101e3c132f172aa4292d0299f2e84
Alina Koptyaeva <alina.koptyaeva@gmail.com>
2020-06-02 10:34:02 +0300

    add useMemo to calc sumAllReviews on component restorant

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import Menu from "./menu";
 import Reviews from "./reviews";
 import Rate from "./rate";
@@ -6,9 +6,11 @@ import Rate from "./rate";
 function Restaurant(props) {
     const numberAllReviews = props.reviews.length;
 
-    let sumAllReviews = props.reviews.reduce((sum, review) => (
-        sum + review.rating
-    ), 0);
+    let sumAllReviews = useMemo(() => (
+        props.reviews.reduce((sum, review) => (
+            sum + review.rating
+        ), 0)
+    ), [props.reviews]);
 
     let averageRating = (sumAllReviews / numberAllReviews).toFixed(1);
 
--- src/components/reviews.js
+++ src/components/reviews.js
@@ -4,11 +4,11 @@ import Rate from "./rate";
 function Reviews(props) {
 
     return (
-        <div className='reviews-grid'>
+        <div>
             <h3>Reviews</h3>
 
             {props.reviews.map((review) => (
-                <div key={review.id} className='reviews-item'>
+                <div key={review.id}>
                     <p>{review.user} - "{review.text}"</p>
                     <p>Rating: {<Rate rating={review.rating} />}</p>
                 </div>
