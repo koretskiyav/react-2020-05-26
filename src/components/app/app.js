d90cef06ad2997bd9446c359cd2dc23612024417
Sergey Zheleznikov <polwen@yandex.ru>
2020-06-04 18:16:32 +0300

    02 check app proptypes

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,9 +1,21 @@
 import React, { PureComponent } from 'react';
 import Restaurants from '../restaurants';
 import Header from '../header';
+import PropTypes from 'prop-types';
 
 export default class App extends PureComponent {
+  static propTypes = {
+    restaurants: PropTypes.arrayOf(),
+  };
+
+  // static propTypes = {
+  //   menu: PropTypes.arrayOf(
+  //     PropTypes.shape({ id: PropTypes.string.isRequired }).isRequired
+  //   ).isRequired,
+  // };
+
   render() {
+    console.log(this.props.restaurants);
     return (
       <div>
         <Header />
