42886c21c520d6f788488c5b4122966cb340be98
Dmitriy Petrukhin <petrukhindmitriy@gmail.com>
2020-06-12 03:11:03 +0300

    fix mutation reviews[] in restaurants reducer

--- src/redux/reducer/restaurants.js
+++ src/redux/reducer/restaurants.js
@@ -1,5 +1,6 @@
 import { normalizedRestaurants } from '../../fixtures';
 import { ADD_REVIEW_TO_RESTAURANT } from '../constants';
+import reviews from './reviews';
 
 const defaultRestaurants = normalizedRestaurants.reduce((acc, restaurant) => {
   return { ...acc, [restaurant.id]: restaurant };
@@ -9,12 +10,12 @@ export default (restaurants = defaultRestaurants, action) => {
   const { type, payload } = action;
   switch (type) {
     case ADD_REVIEW_TO_RESTAURANT:
-      restaurants[payload.restaurantId].reviews = [
-        ...restaurants[payload.restaurantId].reviews,
-        payload.uuid,
-      ];
       return {
         ...restaurants,
+        [restaurants[payload.restaurantId].reviews]: [
+          ...restaurants[payload.restaurantId].reviews,
+          payload.uuid,
+        ],
       };
     default:
       return restaurants;
