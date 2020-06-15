3968e8502ec4b70ae1e42d402dec30585fba26b8
EM Petrova <ekpetrova@ozon.ru>
2020-06-15 19:56:11 +0300

    homework 5 done

--- src/redux/reducer/users.js
+++ src/redux/reducer/users.js
@@ -1,18 +1,50 @@
-import { ADD_REVIEW } from '../constants';
-import { normalizedUsers } from '../../fixtures';
+import {
+  ADD_REVIEW,
+  REQUEST,
+  SUCCESS,
+  FAILURE,
+  LOAD_USERS,
+} from '../constants';
 import { arrToMap } from '../utils';
+import produce from 'immer';
 
-export default (state = arrToMap(normalizedUsers), action) => {
-  const { type, payload, userId } = action;
+const initialState = {
+  entities: {},
+  loading: false,
+  loaded: false,
+  error: null,
+};
+
+export default (state = initialState, action) => {
+  const { type, payload, userId, response, error } = action;
 
   switch (type) {
-    case ADD_REVIEW:
-      const { name } = payload.review;
+    case LOAD_USERS + REQUEST:
       return {
         ...state,
-        [userId]: { id: userId, name },
+        loading: true,
+        error: null,
       };
-
+    case LOAD_USERS + SUCCESS:
+      return {
+        ...state,
+        entities: arrToMap(response),
+        loading: false,
+        loaded: true,
+      };
+    case LOAD_USERS + FAILURE:
+      return {
+        ...state,
+        loading: false,
+        loaded: false,
+        error,
+      };
+    case ADD_REVIEW:
+      const { name } = payload.review;
+      //  draft[reviewId] = { id: reviewId, userId, text, rating };
+      return produce(state, (draft) => {
+        draft.entities[userId] = { id: userId, name };
+      });
     default:
       return state;
   }
