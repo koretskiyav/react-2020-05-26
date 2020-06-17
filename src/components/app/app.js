d57e5f4391547e2210b4efe38141c276b27c32f7
kirill.ilinsky <kirill.ilinsky@gmail.com>
2020-06-17 10:39:02 +0300

    delete old decisions

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,8 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import RestaurantsPage from '../pages/restaurants-page';
 import Header from '../header';
 import Basket from '../basket';
-import Reviews from '../reviews';
-import Menu from '../menu';
+
 export default class App extends PureComponent {
   render() {
     return (
@@ -13,11 +12,6 @@ export default class App extends PureComponent {
         <Switch>
           <Route path="/checkout" component={Basket} />
           <Route path="/restaurants" component={RestaurantsPage} />
-          <Route
-            path="/restaurants/:restaurantId/reviews"
-            component={Reviews}
-          />
-          <Route path="/restaurants/:restaurantId/menu" component={Menu} />
           <Route path="/" render={() => <div>404 - not found</div>} />
         </Switch>
       </div>
