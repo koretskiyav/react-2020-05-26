f886bd678c87e444f7ece429f5c226a7cb0d2ae0
a-shilova <32563685+a-shilova@users.noreply.github.com>
2020-06-18 21:30:06 +0500

    Use link for reviews/menu

--- src/components/restaurant/restaurant.js
+++ src/components/restaurant/restaurant.js
@@ -4,25 +4,41 @@ import Menu from '../menu';
 import Reviews from '../reviews';
 import Banner from '../banner';
 import Rate from '../rate';
-import Tabs from '../tabs';
 import { connect } from 'react-redux';
 import { averageRatingSelector } from '../../redux/selectors';
+import { NavLink, Route } from 'react-router-dom';
+import styles from '../restaurants/restaurants.module.css';
 
 const Restaurant = ({ id, name, menu, reviews, averageRating }) => {
-  const tabs = [
-    { title: 'Menu', content: <Menu menu={menu} restaurantId={id} /> },
-    {
-      title: 'Reviews',
-      content: <Reviews reviews={reviews} restaurantId={id} />,
-    },
-  ];
-
   return (
     <div>
       <Banner heading={name}>
         {!!averageRating && <Rate value={averageRating} />}
       </Banner>
-      <Tabs tabs={tabs} />
+      <div className={styles.tabs}>
+        <NavLink
+          className={styles.tab}
+          to={`/restaurants/${id}/menu`}
+          activeClassName={styles.active}
+        >
+          Menu
+        </NavLink>
+        <NavLink
+          className={styles.tab}
+          to={`/restaurants/${id}/review`}
+          activeClassName={styles.active}
+        >
+          Reviews
+        </NavLink>
+      </div>
+      <Route
+        path={`/restaurants/${id}/menu`}
+        render={() => <Menu menu={menu} restaurantId={id} />}
+      />
+      <Route
+        path={`/restaurants/${id}/review`}
+        render={() => <Reviews reviews={reviews} restaurantId={id} />}
+      />
     </div>
   );
 };
