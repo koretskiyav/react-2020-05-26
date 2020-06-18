83c1a3b9d9c4edb7888edd30b8d234f25e293723
Taras Inzyk <inztar@ya.ru>
2020-06-18 17:34:56 +0500

    1. Сделал роутинг в reviews/menu

--- src/components/restaurant/restaurant.js
+++ src/components/restaurant/restaurant.js
@@ -1,34 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { NavLink, Route } from 'react-router-dom';
+
 import Menu from '../menu';
 import Reviews from '../reviews';
 import Banner from '../banner';
 import Rate from '../rate';
-import Tabs from '../tabs';
-import { connect } from 'react-redux';
+
 import { averageRatingSelector } from '../../redux/selectors';
 
-const Restaurant = ({ id, name, menu, reviews, averageRating }) => {
-  const tabs = [
-    { title: 'Menu', content: <Menu menu={menu} restaurantId={id} /> },
-    {
-      title: 'Reviews',
-      content: <Reviews reviews={reviews} restaurantId={id} />,
-    },
-  ];
+import styles from './restaurant.module.css';
 
+const Restaurant = ({ id, name, menu, reviews, averageRating, url }) => {
   return (
     <div>
       <Banner heading={name}>
         {!!averageRating && <Rate value={averageRating} />}
       </Banner>
-      <Tabs tabs={tabs} />
+      <div className={styles.tabs}>
+        <NavLink
+          to={`${url}/menu`}
+          className={styles.tab}
+          activeClassName={styles.active}
+        >
+          Menu
+        </NavLink>
+        <NavLink
+          to={`${url}/reviews`}
+          className={styles.tab}
+          activeClassName={styles.active}
+        >
+          Reviews
+        </NavLink>
+      </div>
+      <Route
+        path={`${url}/menu`}
+        render={() => <Menu menu={menu} restaurantId={id} />}
+      />
+      <Route
+        path={`${url}/reviews`}
+        render={() => <Reviews reviews={reviews} restaurantId={id} />}
+      />
     </div>
   );
 };
 
 Restaurant.propTypes = {
   id: PropTypes.string,
+  url: PropTypes.string.isRequired,
   name: PropTypes.string,
   menu: PropTypes.array,
   reviews: PropTypes.array,
