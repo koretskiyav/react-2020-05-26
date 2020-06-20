67e36969ad6fdf47585c27c4ad90fe04a9ea60ba
a-shilova <32563685+a-shilova@users.noreply.github.com>
2020-06-20 12:54:14 +0500

    Redirect to restaurant-page

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import RestaurantsPage from '../pages/restaurants-page';
 import Header from '../header';
 import Basket from '../basket';
@@ -20,7 +20,7 @@ const App = () => {
           <Route path="/checkout" component={Basket} />
           <Route path="/restaurants" component={RestaurantsPage} />
           <Route path="/error" render={() => <h1>Error Page</h1>} />
-          <Route path="/" render={() => <div>404 - not found</div>} />
+          <Redirect from="/" to="/restaurants" />
         </Switch>
       </UserProvider>
     </div>
