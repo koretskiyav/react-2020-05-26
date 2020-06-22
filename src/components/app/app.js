d2fed7cd5b412b415b8646d7bdd628b5cfb7d6cf
kirill.ilinsky <kirill.ilinsky@gmail.com>
2020-06-22 11:34:19 +0300

    z

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch,Redirect  } from 'react-router-dom';
 import RestaurantsPage from '../pages/restaurants-page';
 import Header from '../header';
 import Basket from '../basket';
@@ -21,6 +21,7 @@ const App = () => {
           <Route path="/restaurants" component={RestaurantsPage} />
           <Route path="/error" render={() => <h1>Error Page</h1>} />
           <Route path="/" render={() => <div>404 - not found</div>} />
+          <Redirect from='/' to='/restaurants' />
         </Switch>
       </UserProvider>
     </div>
